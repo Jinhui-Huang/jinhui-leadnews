7f50f19ca7c9cae0f303b0c44052225399e7307e
Jinhui-Huang <2634692718@qq.com>
2024-01-02 23:45:00 +0800

    网关模块

---
+++ jinhui-leadnews-gateway/src/main/java/com/myhd/jinhuileadnewsgateway/JinhuiLeadnewsGatewayApplication.java
@@ -0,0 +1,29 @@
+package com.myhd.jinhuileadnewsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Description: JinhuiLeadnewsGatewayApplication
+ * <br></br>
+ * className: JinhuiLeadnewsGatewayApplication
+ * <br></br>
+ * packageName: com.myhd.jinhuileadnewsgateway
+ *
+ * @author jinhui-huang
+ * @version 1.0
+ * @email 2634692718@qq.com
+ * @Date: 2024/1/2 23:41
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JinhuiLeadnewsGatewayApplication {
+    public JinhuiLeadnewsGatewayApplication() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinhuiLeadnewsGatewayApplication.class, args);
+    }
+}
---
+++ jinhui-leadnews-gateway/src/main/java/com/myhd/jinhuileadnewsgateway/filter/AuthorizeFilter.java
@@ -0,0 +1,69 @@
+package com.myhd.jinhuileadnewsgateway.filter;
+
+import com.myhd.jinhuileadnewsutils.utils.common.AppJwtUtil;
+import io.jsonwebtoken.Claims;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+/**
+ * Description: AuthorizeFilter
+ * <br></br>
+ * className: AuthorizeFilter
+ * <br></br>
+ * packageName: com.myhd.jinhuileadnewsgateway.filter
+ *
+ * @author jinhui-huang
+ * @version 1.0
+ * @email 2634692718@qq.com
+ * @Date: 2024/1/2 23:38
+ */
+
+@Component
+public class AuthorizeFilter implements Ordered, GlobalFilter {
+    private static final Logger log = LoggerFactory.getLogger(AuthorizeFilter.class);
+
+    public AuthorizeFilter() {
+    }
+
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        if (request.getURI().getPath().contains("/login")) {
+            return chain.filter(exchange);
+        } else {
+            String token = request.getHeaders().getFirst("token");
+            if (StringUtils.isBlank(token)) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            } else {
+                try {
+                    Claims claimsBody = AppJwtUtil.getClaimsBody(token);
+                    int result = AppJwtUtil.verifyToken(claimsBody);
+                    if (result == 1 || result == 2) {
+                        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return response.setComplete();
+                    }
+                } catch (Exception var8) {
+                    var8.printStackTrace();
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return response.setComplete();
+                }
+
+                return chain.filter(exchange);
+            }
+        }
+    }
+
+    public int getOrder() {
+        return 0;
+    }
+}
